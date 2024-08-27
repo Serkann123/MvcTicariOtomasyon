3ab91bd9c14311f63640035e14488fdc5de42905
Serkann123 <serkanaltunay0203@gmail.com>
2024-08-27 16:13:08 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Personel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Personel
+    {
+        [Key]
+        public int Personelİd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string PersonelAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(10)]
+        public string personelSoyad { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(250)]
+        public string personelGorsel { get; set; }
+
+        public SatisHareket SatisHareket { get; set; }
+        public Departman Departman { get; set; }
+    }
+}
