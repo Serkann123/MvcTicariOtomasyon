ad46f9186b89c3f1153878d522914f08740bc4a4
Serkan Altunay <serkanaltunay0203@gmail.com>
2024-09-26 15:52:50 +0300

    .

---
+++ MvcOnlineTicariOtomasyon/Controllers/ÜrünDetayController.cs
@@ -0,0 +1,20 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class ÜrünDetayController : Controller
+    {
+        Context c = new Context();
+
+        public ActionResult Index()
+        {
+            var degerler = c.Uruns.Where(x => x.Urunİd == 1).ToList();
+            return View(degerler);
+        }
+    }
+}
