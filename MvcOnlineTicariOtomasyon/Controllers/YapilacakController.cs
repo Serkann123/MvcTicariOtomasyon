6d7f50b457de802d70cd682ae2e511f5df43bb1b
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2024-10-19 02:13:14 +0300

    .

--- MvcOnlineTicariOtomasyon/Controllers/YapilacakController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/YapilacakController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,25 @@ namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class YapilacakController : Controller
     {
+        Context c = new Context();
+
         public ActionResult Index()
         {
-            return View();
+            var values1 = c.Carilers.Count().ToString();
+            ViewBag.v1 = values1;
+
+            var values2 = c.Uruns.Count().ToString();
+            ViewBag.v2 = values2;
+
+            var values3 = c.Kategoris.Count().ToString();
+            ViewBag.v3 = values3;
+
+            var values4 = (from x in c.Carilers select x.CariSehir).Distinct().Count().ToString(); 
+            ViewBag.v4 = values4;
+
+            var values = c.Yapilacaks.ToList();
+
+            return View(values);
         }
     }
 }
